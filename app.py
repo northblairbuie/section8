bd50e095446394107fe0b2efd47a4ee179f62026
Mark Henderson <northblairbuie@gmail.com>
2021-09-16 20:58:20 +0100

    moved db import

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from db import db
+
 from flask import Flask
 from flask_restful import  Api
 from flask_jwt import JWT
@@ -12,10 +12,12 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+print("--------")
 print(os.environ.get('DATABASE_URL'))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vfgnbwvafppghc:743e5c22d0456d0bf6fc5893f859aec1f0c3cb0d6f07e49e655ecb559b0d6287@ec2-54-73-147-133.eu-west-1.compute.amazonaws.com:5432/d44jn1bop0445j'
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(['DATABASE_URL'])#, 'sqlite:///data.db')
-print('SQLALCHEMY_DATABASE_URI')
+print(app.config['SQLALCHEMY_DATABASE_URI'])
+print("--------")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # turns off flask sqlalchemy mod tracking
@@ -32,5 +34,6 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
